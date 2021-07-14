a2be75acb52d39b61df39a94d4236d1dc8cd745f
kam3r0nz <kam.hungerford@gmail.com>
2021-07-14 18:33:21 -0500

    Installed redux, react-redux, redux-devtools-extension, and redux-thunk libraries

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 
 ReactDOM.render(
   <React.StrictMode>
