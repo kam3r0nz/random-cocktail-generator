37327afaa51b41d5f60f4f47feb61bd3a7c9aeb7
kam3r0nz <kam.hungerford@gmail.com>
2021-08-18 16:39:33 -0500

    Created addRecipe action

--- src/actions/recipeActions.js
+++ src/actions/recipeActions.js
@@ -3,6 +3,10 @@ export function fetchRecipe() {
         dispatch({type: 'FETCH_RECIPE'})
         fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
             .then(resp => resp.json())
-            .then(recipe => console.log(recipe))
+            .then(recipe => dispatch({type: 'ADD_RECIPE', recipe}))
     }
+}
+
+export function addRecipe(recipe) {
+    return dispatch => dispatch({type: 'ADD_RECIPE', recipe})
 }
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,6 +2,8 @@ export default function rootReducer(state = [], action) {
     switch(action.type){
         case 'FETCH_RECIPE':
             return [...state]
+        case 'ADD_RECIPE':
+            return [...state, action.recipe]
         default:
             return state
     }
